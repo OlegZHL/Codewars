ddefe16a533731bb37b99f489d71e78d50eb6392
OlegZHL <olegZHL@yandex.ru>
2025-02-01 23:05:48 +0300

    project

---
+++ 8kyu/Training JS #1 create your first JS function and print Hello World!.js
@@ -0,0 +1,27 @@
+// Please refer to two example above and write your first JS function.
+// mission 1:
+// use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+// mission 2:
+// use keyword var (or let or const) to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+// mission 3:
+// type the console.log() in the next line (don't forget to put the str in the parentheses).
+// When you have finished the work, click "Run Tests" to see if your code is working properly.
+// In the end, click "Submit" to submit your code pass this kata.
+
+
+
+// Пожалуйста, обратитесь к двум примерам выше и напишите свою первую JS-функцию.
+// миссия 1:
+// используйте ключевое слово function для определения вашей функции, имя функции должно быть helloWorld (не забудьте про () and {})
+// миссия 2:
+// используйте ключевое слово var (или let или const) для определения переменной str, значение str должно быть строкой: "Hello World!" (не забудьте =).
+// миссия 3:
+// Введите console.log() в следующей строке (не забудьте поставить str в скобки).
+// Когда вы закончите работу, нажмите «Запустить тесты», чтобы проверить, правильно ли работает ваш код.
+// В конце нажмите «Отправить», чтобы отправить свой код и пройти это задание.
+
+
+function helloWorld() {
+    let str = "Hello World!";
+    console.log(str);
+}
