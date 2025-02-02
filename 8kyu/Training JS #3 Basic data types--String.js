77d3536b27e7554e3c8f585f9d61f01dc3fe04ed
OlegZHL <olegZHL@yandex.ru>
2025-02-02 21:27:38 +0300

    project

---
+++ 8kyu/Training JS #3 Basic data types--String.js
@@ -0,0 +1,44 @@
+// misson 1: I've create three function, and defined some global variables, please select some variables that can make up the name of the function, 
+// and return them(Please note the uppercase and lowercase letters are different).
+// When you have finished the work, click "Run Tests" to see if your code is working properly.
+// misson 2: After misson 1 finished. you should click "Attempt" to see my three questions, and write the answer in function answer1, 
+// answer2,answer3
+// If everything is right, click "Submit" again to submit your code pass this kata.
+
+
+// Задание 1: я создал три функции и определил несколько глобальных переменных. Пожалуйста, выберите несколько переменных, 
+// которые могут составлять имя функции, и верните их (обратите внимание, что прописные и строчные буквы отличаются).
+// Когда вы закончите работу, нажмите «Запустить тесты», чтобы проверить, правильно ли работает ваш код.
+// задание 2: после выполнения задания 1. вам нужно нажать «Попытаться», чтобы увидеть мои три вопроса, и написать ответ в функцииanswer1, 
+// answer2,answer3
+// Если всё верно, нажмите «Отправить» ещё раз, чтобы отправить код и пройти это задание.
+
+
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+function Dad(){
+  //select some variable to combine "Dad"
+  return d1 + a2 + d2;
+}
+function Bee(){
+  //select some variable to combine "Bee"
+  return b1 + e2 + e2;
+}
+function banana(){
+  //select some variable to combine "banana"
+  return b2 + a2 + n2 + a2 + n2 + a2;
+}
+
+//answer some questions if you finished works above
+function answer1(){
+  //the answer should be "yes" or "no"
+  return "no";
+}
+function answer2(){
+  //the answer should be "yes" or "no"
+  return "no";
+}
+function answer3(){
+  //the answer should be "yes" or "no"
+  return "yes";
+}
+
