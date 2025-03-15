468f79ea42fa640249ef0afe69dddbba608b0129
OlegZHL <olegZHL@yandex.ru>
2025-03-15 23:53:55 +0300

    project

---
+++ 8kyu/Training JS #18 Methods of String object--concat() split() and its good friend join().js
@@ -0,0 +1,32 @@
+// Task
+// Implement a function which accepts 2 arguments: string and separator.
+
+// The expected algorithm: split the string into words by spaces, split each word into separate 
+// characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces.
+
+// For example:
+
+// splitAndMerge("My name is John", " ")  ==  "M y n a m e i s J o h n"
+// splitAndMerge("My name is John", "-")  ==  "M-y n-a-m-e i-s J-o-h-n"
+// splitAndMerge("Hello World!", ".")     ==  "H.e.l.l.o W.o.r.l.d.!"
+// splitAndMerge("Hello World!", ",")     ==  "H,e,l,l,o W,o,r,l,d,!"
+
+
+// Задача
+// Реализуйте функцию, которая принимает 2 аргумента: string и separator.
+
+// Ожидаемый алгоритм: разделите string на слова по пробелам, разделите каждое слово на отдельные символы и соедините их 
+// с указанным separator, соедините все полученные «слова» в предложение с помощью пробелов.
+
+// Например:
+
+// splitAndMerge("My name is John", " ")  ==  "M y n a m e i s J o h n"
+// splitAndMerge("My name is John", "-")  ==  "M-y n-a-m-e i-s J-o-h-n"
+// splitAndMerge("Hello World!", ".")     ==  "H.e.l.l.o W.o.r.l.d.!"
+// splitAndMerge("Hello World!", ",")     ==  "H,e,l,l,o W,o,r,l,d,!"
+
+
+
+
+
+
