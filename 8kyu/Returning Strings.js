6f6b371b34af5c291096dda65829e4c9d0b68495
OlegZHL <olegZHL@yandex.ru>
2025-04-19 21:37:17 +0300

    project

---
+++ 8kyu/Returning Strings.js
@@ -0,0 +1,15 @@
+// Create a function that accepts a parameter representing a name and returns the message:
+//     "Hello, <name> how are you doing today?".
+//
+//     [Make sure you type the exact thing I wrote or the program may not execute properly]
+//
+// Создайте функцию, которая принимает параметр, представляющий собой name и возвращает сообщение:
+//     "Hello, <name> how are you doing today?".
+//
+//     [Убедитесь, что вы вводите именно то, что я написал, иначе программа может работать некорректно]
+
+function greet(name){
+    return  `Hello, ${name} how are you doing today?`;
+}
+
+console.log(greet("Julia"));
