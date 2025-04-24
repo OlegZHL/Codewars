86f913fa680582da539184ed4b557d9d5b3af129
OlegZHL <olegZHL@yandex.ru>
2025-04-24 22:39:33 +0300

    project

---
+++ 8kyu/Sum of positive.js
@@ -0,0 +1,28 @@
+// Task
+// You get an array of numbers, return the sum of all of the positives ones.
+//
+//     Example
+//     [1, -4, 7, 12] =>1 + 7 + 12 = 20
+//
+// Note
+// If there is nothing to sum, the sum is default to 0.
+//
+// Задача
+// Вы получаете массив чисел и возвращаете сумму всех положительных чисел.
+//
+//     Пример
+//     [1, -4, 7, 12] =>1+7+12=20
+// Примечание
+// Если суммировать нечего, сумма по умолчанию равна 0.
+
+function positiveSum(arr) {
+    let sum = 0;
+     arr.filter((item) => {
+        if(item>0){
+          return  sum+=item;
+        }
+    });
+    return sum;
+}
+
+console.log(positiveSum([1, -4, 7, 12]));
