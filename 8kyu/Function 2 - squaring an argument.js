7f2075e258c0589326992c0a352b59a50b7feebd
OlegZHL <olegZHL@yandex.ru>
2025-05-04 22:45:57 +0300

    project

---
+++ 8kyu/Function 2 - squaring an argument.js
@@ -0,0 +1,9 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+//
+// Теперь вам нужно написать функцию, которая принимает аргумент и возвращает его квадрат.
+
+function square(n) {
+    return Math.pow (n, 2)
+}
+
+console.log(square(5));
