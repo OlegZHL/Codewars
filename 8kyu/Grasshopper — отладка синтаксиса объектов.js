4842650c8228fbeb84ffbdfc845b2784c6fe490f
OlegZHL <olegZHL@yandex.ru>
2025-07-19 23:22:09 +0300

    project

---
+++ 8kyu/Grasshopper — отладка синтаксиса объектов.js
@@ -0,0 +1,21 @@
+// Отладка объекта
+// При создании игры в стиле Zork вы создаёте объект rooms.
+// К сожалению, игра не работает. Найдите все ошибки в объекте rooms, чтобы игра снова заработала.
+
+let rooms = {
+    first: {
+        description: 'This is the first room',
+        items: {
+            chair: 'The old chair looks comfortable',
+            lamp: 'This lamp looks ancient'
+        },
+    },
+        second: {
+            description: 'This is the second room',
+            items: {
+                couch: 'This couch looks like it would hurt your back',
+                table: 'On the table there is an unopened bottle of water'
+            }
+        }
+}
+
