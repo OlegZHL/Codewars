c818c900565eab62ddb43a0da294b06aa0fe12eb
OlegZHL <olegZHL@yandex.ru>
2025-08-09 00:18:07 +0300

    project

---
+++ 8kyu/Kata Example Twist.js
@@ -0,0 +1,10 @@
+// Это простая вариация на тему ката (предоставляется Codewars при изучении того,
+// как создавать собственные ката).
+//
+// Добавьте в массив значение "codewars" websites 1000 раз.
+
+let websites = [];
+while(websites.length !== 1000){
+    websites.push("codewars");
+}
+console.log(websites);
