5a81baa5731117b8f5d70bd90b3cdfd3cc672eb3
OlegZHL <olegZHL@yandex.ru>
2025-09-20 22:45:58 +0300

    project

--- 8kyu/Smallest unused ID.js
+++ 8kyu/Smallest unused ID.js
@@ -12,7 +12,11 @@
 // Давай, напиши что-нибудь потрясающее!
 
 function nextId(ids){
-   return ids;
+    let id = 0;
+    while(ids.includes(id)){
+          id++;
+    }
+    return id;
 }
 
-console.log(nextId([0,1,2,3,4,5,6,7,8,9,10]));
+console.log(nextId([1,0,2,3,4,5,5,7,8,9,10]));
