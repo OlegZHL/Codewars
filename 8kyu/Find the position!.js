d45d81ed397fe7ed31ffa97813652ff12724485f
OlegZHL <olegZHL@yandex.ru>
2025-10-13 12:11:33 +0300

    project

--- 8kyu/Find the position!.js
+++ 8kyu/Find the position!.js
@@ -6,5 +6,10 @@
 // Примечание: проверяются только строчные буквы английского алфавита
 
 function position(letter){
-    //Write your own Code!
+    let obj = {a:1,b:2,c:3,d:4,e:5,f:6,g:7,h:8,i:9,j:10,k:11,l:12,m:13,
+               n:14,o:15,p:16,q:17,r:18,s:19,t:20,u:21,v:22,w:23,x:24,y:25,z:26};
+    let key = Object.keys(obj).find(item => item === letter.toLowerCase());
+    return `Position of alphabet: ${obj[key]}`;
 }
+
+console.log(position("Y"));
