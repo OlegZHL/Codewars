80385cf354353cd66c1ddada19065258ca144381
OlegZHL <olegZHL@yandex.ru>
2025-10-18 22:55:44 +0300

    project

--- 8kyu/Merging sorted integer arrays (without duplicates).js
+++ 8kyu/Merging sorted integer arrays (without duplicates).js
@@ -2,6 +2,9 @@
 // Массивы содержат только целые числа.
 // Кроме того, конечный результат должен быть отсортирован и не должен содержать дубликатов.
 
-function mergeArrays(a, b) {
-    // your code here
+function mergeArrays(a, b){
+    return a.concat(b).sort((a, b) => a - b).
+    filter((item, index, array) => array.indexOf(item) === index);
 }
+
+console.log(mergeArrays([2, 4, 8], [2, 4, 6]));
