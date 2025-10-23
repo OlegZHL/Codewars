f6196f5a97b44cd4a5dab9db234dd56d546641b5
OlegZHL <olegZHL@yandex.ru>
2025-10-23 23:14:44 +0300

    project

--- 8kyu/Sort My Textbooks.js
+++ 8kyu/Sort My Textbooks.js
@@ -6,9 +6,14 @@
 //
 // Сортировка НЕ должна зависеть от регистра
 
-
-function sorter(textbooks){
-    return textbooks.sort((a,b)=> a.localeCompare(b,"en", {'sensitivity':'base'}));
+function sorter(textbooks) {
+    return textbooks.sort((a, b) => {
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        if (a < b) return -1;
+        if (a > b) return 1;
+        return 0;
+    });
 }
 
-console.log(sorter( ['Algebra', 'history', 'Geometry', 'english']));
+console.log(sorter( ['Alg#bra', '$istory', 'Geom^try', '**english']));
