c81e53073e7a4426aae0d10a602d67abefba0773
OlegZHL <olegZHL@yandex.ru>
2025-11-02 23:39:34 +0300

    project

---
+++ 8kyu/Logical calculator.js
@@ -0,0 +1,26 @@
+// Дан массив логических значений и логический оператор.
+// Верните логическое значение, полученное в результате последовательного
+// применения оператора к значениям в массиве.
+//
+// Примеры
+// логические значения = [True, True, False], оператор = "AND"
+// True AND True -> True
+// True AND False -> False
+// Возврат False
+// логические значения = [True, True, False], оператор = "OR"
+// True OR True -> True
+// True OR False -> True
+// Возврат True
+// логические значения = [True, True, False], оператор = "XOR"
+// True XOR True -> False
+// False XOR False -> False
+// Возврат False
+// Входные данные
+// массив логических значений (1 <= array_length <= 50)
+// строка, указывающая на логический оператор: "AND", "OR", "XOR"
+// Выходной сигнал
+// Логическое значение (True или False).
+
+function logicalCalc(array, op){
+    //your code here
+}
