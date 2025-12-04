5b263024429327ab2dec7ec562fd2bf5c102abdf
OlegZHL <olegZHL@yandex.ru>
2025-12-04 23:28:53 +0300

    project

---
+++ 8kyu/Price of Mangoes.js
@@ -0,0 +1,34 @@
+// Время бухгалтера! Для заданного количества и цены (за манго) рассчитайте общую стоимость манго.
+// Но! Каждое третье манго бесплатно!
+//
+//     Примеры
+// # 🥭 : Paid mango
+// # 🆓 : Free mango
+//
+// Quantity = 2
+// Price = 3
+// Total cost ==> 6
+// # Paid 2 mangoes for $3 per unit = $6; no mango for free
+//     # 🥭🥭
+//
+// Quantity = 3
+// Price = 3
+// Total cost ==> 6
+// # Paid 2 mangoes for $3 per unit = $6; +1 mango for free
+//     # 🥭🥭🆓
+//
+// Quantity = 5
+// Price = 3
+// Total cost ==> 12
+// # Paid 4 mangoes for $3 per unit = $12; +1 mango for free
+//     # 🥭🥭🆓   🥭🥭
+//
+// Quantity = 9
+// Price = 5
+// Total cost ==> 30
+// # Paid 6 mangoes for $5 per unit = $30; +3 mangoes for free
+//     # 🥭🥭🆓   🥭🥭🆓   🥭🥭🆓
+
+function mango(quantity, price){
+
+}
