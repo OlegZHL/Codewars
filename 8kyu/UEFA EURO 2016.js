8a02d6f3f6c95ac4eef496cf13d122d6af4c94a9
OlegZHL <olegZHL@yandex.ru>
2025-12-09 00:31:38 +0300

    project

--- 8kyu/UEFA EURO 2016.js
+++ 8kyu/UEFA EURO 2016.js
@@ -6,4 +6,13 @@
 
 
 function uefaEuro2016(teams, scores){
+    if(scores[0] > scores[1]){
+        return `At match ${teams[0]} - ${teams[1]}, ${teams[0]} won!`;
+    }else if(scores[1] > scores[0]){
+        return `At match ${teams[0]} - ${teams[1]}, ${teams[1]} won!`;
+    }else if(scores[0] === scores[1]){
+        return `At match ${teams[0]} - ${teams[1]}, teams played draw.`;
+    }
 }
+
+console.log(uefaEuro2016(['Portugal', 'Iceland'],[1, 1]));
