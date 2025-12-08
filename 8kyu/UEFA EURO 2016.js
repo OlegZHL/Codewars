c67f74e92ba8966176fcaf16bd764988ae249532
OlegZHL <olegZHL@yandex.ru>
2025-12-08 23:28:55 +0300

    project

---
+++ 8kyu/UEFA EURO 2016.js
@@ -0,0 +1,9 @@
+// Доработайте функцию uefaEuro2016(), чтобы она возвращала строку, как в примерах ниже:
+//
+// uefaEuro2016(['Germany', 'Ukraine'],[2, 0]) // "At match Germany - Ukraine, Germany won!"
+// uefaEuro2016(['Belgium', 'Italy'],[0, 2]) // "At match Belgium - Italy, Italy won!"
+// uefaEuro2016(['Portugal', 'Iceland'],[1, 1]) // "At match Portugal - Iceland, teams played draw."
+
+
+function uefaEuro2016(teams, scores){
+}
