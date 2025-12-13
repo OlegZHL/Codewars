fb1beaa4879eca5537dd6a9dd9e10f1866956b86
OlegZHL <olegZHL@yandex.ru>
2025-12-13 23:48:30 +0300

    project

--- 8kyu/Fundamentals Return.js
+++ 8kyu/Fundamentals Return.js
@@ -13,25 +13,25 @@
 // Примечание: все математические операции будут выглядеть так: a (операция) b
 
 function add(a,b){
-    return
+    return a+b;
 }
 
 function divide(a,b){
-    return
+    return a/b;
 }
 
 function multiply(a,b){
-    return
+    return a*b;
 }
 
 function mod(a,b){
-    return
+    return a%b;
 }
 
 function exponent(a,b){
-    return
+    return a**b;
 }
 
 function subt(a,b){
-    return
+    return a-b;
 }
