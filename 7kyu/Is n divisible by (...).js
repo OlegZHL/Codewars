f4f2ade822a1363616e406941c007b03e7366b05
OlegZHL <olegZHL@yandex.ru>
2026-01-11 20:38:39 +0300

    project

---
+++ 7kyu/Is n divisible by (...).js
@@ -0,0 +1,13 @@
+// Создайте функцию, которая проверяет, делится ли первый аргумент n на все остальные аргументы
+// (возвращает true, если других аргументов нет)
+//
+// Пример:
+//
+// (6,1,3)--> true because 6 is divisible by 1 and 3
+// (12,2)--> true because 12 is divisible by 2
+// (100,5,4,10,25,20)--> true
+// (12,7)--> false because 12 is not divisible by 7
+
+function isDivisible(){
+    //Write your code here
+}
