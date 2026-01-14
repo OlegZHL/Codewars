5607588f35ad5b9d0e602fee1b7f5ff2ca152864
OlegZHL <olegZHL@yandex.ru>
2026-01-15 00:21:54 +0300

    project

--- 7kyu/16+18=214.js
+++ 7kyu/16+18=214.js
@@ -5,8 +5,31 @@
 // Вы можете считать, что оба числа положительные.
 
 function add(num1, num2) {
-    return num1 + num2;
+        let num11 = num1.toString();
+        let num22 = num2.toString();
+
+        if (num11.length > num22.length) {
+            while (num11.length > num22.length) {
+                num22 = "0" + num22;
+            }
+        }
+
+        if (num22.length > num11.length) {
+            while (num22.length > num11.length) {
+                num11 = "0" + num11;
+            }
+        }
+
+        let number1 = num11.split("");
+        let number2 = num22.split("");
+
+        let sum = "";
+        let sumFor= 0;
+            for (let i = 0; i < number1.length; i++) {
+                sumFor = Number(number1[i]) + Number(number2[i]);
+                sum = sum + sumFor.toString();
+            }
+    return parseInt(sum);
 }
 
-console.log(add(38810,1383));
-
+console.log(add(38810, 1383));
