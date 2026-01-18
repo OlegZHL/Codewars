e27b718e213cb007b4a6bf4d74366022b33d3c54
OlegZHL <olegZHL@yandex.ru>
2026-01-18 21:42:14 +0300

    project

--- 7kyu/Double Trouble.js
+++ 7kyu/Double Trouble.js
@@ -13,7 +13,15 @@
 //     Верните полученный массив.
 
 function trouble(x, t){
-
+    let i = 0;
+    while (i < x.length) {
+        if (x[i] + x[i + 1] === t) {
+            x.splice(i + 1, 1);
+        } else {
+            i++;
+        }
+    }
+    return x;
 }
 
-console.log(trouble([2, 2, 2, 2, 2, 2],4));
+console.log(trouble([2, 6, 2],8));
