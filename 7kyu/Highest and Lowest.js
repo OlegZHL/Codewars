821a4c04a1b173952beb1488c475d1e611d9282e
OlegZHL <olegZHL@yandex.ru>
2026-01-21 22:49:12 +0300

    project

--- 7kyu/Highest and Lowest.js
+++ 7kyu/Highest and Lowest.js
@@ -8,7 +8,22 @@
 
 
 function highAndLow(numbers){
-    // ...
+    let arr = numbers.split(' ');
+    let max = Number(arr[0]);
+    let min = Number(arr[0]);
+
+    for (let i = 1; i < arr.length; i++){
+        if(max < Number(arr[i])){
+            max = Number(arr[i]);
+        }
+    }
+
+    for (let i = 1; i < arr.length; i++){
+        if(min > Number(arr[i])){
+            min = Number(arr[i]);
+        }
+    }
+    return `${max} ${min}`;
 }
 
 console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
