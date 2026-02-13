44e3c045183e5879a5e8a7894caf5957d0a4df1b
OlegZHL <olegZHL@yandex.ru>
2026-02-13 22:41:53 +0300

    project

---
+++ 7kyu/Is Undefined.js
@@ -0,0 +1,8 @@
+// Функция должна проверять, является ли введенное значение undefined,
+// но она работает некорректно. Можете ли вы исправить код?
+
+function isUndefined(value) {
+    return value === undefined;
+}
+
+console.log(isUndefined(undefined));
