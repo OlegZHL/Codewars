fe6b852fcc24ff30f8d387ccd767a62a9c4de785
OlegZHL <olegZHL@yandex.ru>
2026-02-25 22:46:43 +0300

    project

---
+++ 8kyu/Grasshopper - Messi Goals.js
@@ -0,0 +1,25 @@
+// Общее количество голов Месси
+// Используйте переменные, чтобы найти сумму голов, забитых Месси в трех турнирах.
+//
+// Информация
+// Статистика голов Месси:
+//
+//     Конкуренция	  Цели
+//   La Liga	      43
+//   Лига чемпионов	  10
+//   Copa del Rey	  5
+//
+// Задача
+// Создайте эти три переменные и сохраните соответствующие значения, используя приведенную выше таблицу:
+// laLigaGoals
+// championsLeagueGoals
+// copaDelReyGoals
+// Создайте четвертую переменную с именем totalGoals, в которой будет храниться сумма всех голов Месси в этом году.
+
+
+let laLigaGoals = 43;
+let championsLeagueGoals = 10;
+let copaDelReyGoals = 5;
+
+let totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+console.log(totalGoals);
